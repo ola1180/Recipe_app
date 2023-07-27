2242e71d0963061eaefb2ed794649c02ff55b28e
Ola <aleksandra.kurdel@gmail.com>
2023-07-27 10:10:32 +0200

    added gitignore file

--- recipes/urls.py
+++ recipes/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('category/<int:pk>/', views.category, name="category"),
     path('recipe/new/', views.recipe_new, name='recipe_new'),
     path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin')
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -82,3 +82,19 @@ def signup(request):
 
     else:
         return render(request, '../templates/recipes/signup.html', {'signup': signup})
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials invalid')
+            return redirect('signin')
+    else:
+        return render(request, '../templates/recipes/signin.html', {'signin': signin})
